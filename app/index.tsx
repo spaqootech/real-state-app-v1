48ab5e6f26d03603e3255c5dbde559e97b3e91df
Spaqoo Tech <mohdalimeer13@gmail.com>
2025-01-03 23:31:20 +0500

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import '../global.css';
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <div className="bg-slate-100 rounded-xl">
+    <p className="text-5xl font-medium text-blue-500">Welcome to Tailwind</p>
+  </div>
+  );
+}
